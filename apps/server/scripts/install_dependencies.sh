58e39fc3ff2852431973d6bde2a5600e435d712d
Quang Pham <quangpham.245@protonmail.com>
2022-11-01 22:11:59 +0700

    Fix CodeDeploy config

--- apps/server/scripts/install_dependencies.sh
+++ apps/server/scripts/install_dependencies.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# Install NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash\
-
-# Reset Bash profile to update NVM configuration
-source /home/ubuntu/.bash_profile
-
-# Install Node 16 and set it as the default version
-nvm install 16
-nvm alias default 16
+# Node version and NPM version
+node --version
+npm --version
 
 # Install PNPM
-pnpm install --global pnpm
-pnpm install --global pm2
+npm install --global pnpm
+npm install --global pm2
