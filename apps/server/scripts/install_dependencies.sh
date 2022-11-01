f5b24f4d839346e4f6cb586e694f1cf4ea218f48
Quang Pham <quangpham.245@protonmail.com>
2022-11-01 19:07:42 +0700

    Fix CodeDeploy config

--- apps/server/scripts/install_dependencies.sh
+++ apps/server/scripts/install_dependencies.sh
@@ -2,8 +2,9 @@
 
 # Install NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash\
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+# Reset Bash profile to update NVM configuration
+source ~/.bashrc
 
 # Install Node 16 and set it as the default version
 nvm install 16
