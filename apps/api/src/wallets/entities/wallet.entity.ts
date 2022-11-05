cbe976d27946f5d4a0ed7f9b2f9296284d92e077
Quang Pham <quangpham.245@protonmail.com>
2022-11-05 18:13:15 +0700

    Add wallet entity

--- apps/api/src/wallets/entities/wallet.entity.ts
+++ apps/api/src/wallets/entities/wallet.entity.ts
@@ -1,1 +1,14 @@
-export class Wallet {}
+import { Column, ManyToOne } from 'typeorm'
+import { BaseEntity } from 'src/common/base.entity'
+import { User } from 'src/users'
+
+export class Wallet extends BaseEntity {
+  @Column({ unique: true })
+  name: string
+
+  @Column()
+  userId: number
+
+  @ManyToOne(() => User)
+  user: User
+}
