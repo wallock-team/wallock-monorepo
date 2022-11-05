ee72eb564d12ca49b9ef645e6adcfbc00fb9cd99
Quang Pham <quangpham.245@protonmail.com>
2022-11-05 17:32:54 +0700

    Remove redundent code in app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,31 +1,18 @@
 import { Module } from '@nestjs/common'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-import { User } from './users/entities/user.entity'
-import { Category } from './categories/entities/category.entity'
 import { UsersModule } from './users/users.module'
-import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
-import AuthController from './auth/auth.controller'
-import { UsersController } from './users/users.controller'
 import { CategoriesModule } from './categories/categories.module'
-import { CategoriesController } from './categories/categories.controller'
 import { TransactionsModule } from './transactions/transactions.module'
 import { DbModule } from './db'
+import { WalletsModule } from './wallets/wallets.module'
 @Module({
   imports: [
     DbModule,
     UsersModule,
     AuthModule,
     CategoriesModule,
-    TransactionsModule
-  ],
-  controllers: [
-    AppController,
-    AuthController,
-    UsersController,
-    CategoriesController
+    TransactionsModule,
+    WalletsModule
   ]
 })
 export class AppModule {}
